95470bebe92d70aeedfde7da105412d66a0221d6
Fabian Schuh <Fabian@chainsquad.com>
2018-04-24 15:17:45 +0200

    Make sure to not forward proposer

--- bookied_sync/lookup.py
+++ bookied_sync/lookup.py
@@ -56,6 +56,8 @@ class Lookup(dict, BlockchainInstance):
     ):
         """ Let's load all the data from the folder and its subfolders
         """
+        kwargs.pop("proposer", None)  # Do not forward proposer
+        kwargs.pop("approver", None)  # Do not forward approver
         BlockchainInstance.__init__(self, *args, **kwargs)
         # self._cwd = os.path.dirname(os.path.realpath(__file__))
         self._cwd = os.getcwd()
